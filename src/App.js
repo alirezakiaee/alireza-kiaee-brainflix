34da6ffc2afc4c8f5ec2a836aee11f35f456e0bd
alirezakiaee <alirezakiaee91@gmail.com>
2023-12-12 22:30:48 -0500

    Routes added to App.Js - Home and Upload route added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import Nav from './components/Nav/Nav'
-import Main from './components/Main/Main'
+import Nav from './components/Nav/Nav';
+import Main from './components/Main/Main';
+import UploadPage from './pages/Upload/Upload';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.scss';
 
-
-
-
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <header className="header">
         <Nav />
       </header>
-      <Main />
-    </>
+      <main className="main">
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/:id" element={<Main />} />
+        </Routes>
+      </main> 
+    </BrowserRouter>
   );
 }
 
