dfe28a69913d185c8aeeaf6148ce8fc0c70a053e
Alireza Kiaee <45744021+alirezakiaee@users.noreply.github.com>
2023-12-17 13:28:35 -0500

    Update VideoList.jsx
    videoList tested - working

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,31 +1,3 @@
-// import VideoListCard from '../VideoListCard/VideoListCard'
-// import { Link } from "react-router-dom";
-// import './VideoList.scss'
-
-// const VideoList = ({ videoData, selected, handleClick }) => {
-//     return (
-//         <section className="video-list">
-//             <h2 className="video-list__title">NEXT VIDEO</h2>
-//             {videoData.map(video => {
-//                 if (video !== selected) {
-//                     return (
-//                         <Link to={`/video/${video.id}`} key={video.id}>
-//                             <VideoListCard
-//                                 videoData={video}
-//                                 handleClick={() => handleClick(video.id)} />
-//                         </Link>
-//                     );
-//                 } else {
-//                     return null;
-//                 }
-//             })}
-//         </section>
-//     )
-// }
-
-// export default VideoList
-
-
 import React from 'react';
 import VideoListCard from '../VideoListCard/VideoListCard';
 import { Link } from 'react-router-dom';
