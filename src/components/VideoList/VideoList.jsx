bea1a53926c743b52547632ee9d7a4b846b8a9b3
alirezakiaee <alirezakiaee91@gmail.com>
2023-12-17 13:25:52 -0500

    Video list issue fixed

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,26 +1,52 @@
-import VideoListCard from '../VideoListCard/VideoListCard'
-import { Link } from "react-router-dom";
-import './VideoList.scss'
+// import VideoListCard from '../VideoListCard/VideoListCard'
+// import { Link } from "react-router-dom";
+// import './VideoList.scss'
+
+// const VideoList = ({ videoData, selected, handleClick }) => {
+//     return (
+//         <section className="video-list">
+//             <h2 className="video-list__title">NEXT VIDEO</h2>
+//             {videoData.map(video => {
+//                 if (video !== selected) {
+//                     return (
+//                         <Link to={`/video/${video.id}`} key={video.id}>
+//                             <VideoListCard
+//                                 videoData={video}
+//                                 handleClick={() => handleClick(video.id)} />
+//                         </Link>
+//                     );
+//                 } else {
+//                     return null;
+//                 }
+//             })}
+//         </section>
+//     )
+// }
+
+// export default VideoList
+
+
+import React from 'react';
+import VideoListCard from '../VideoListCard/VideoListCard';
+import { Link } from 'react-router-dom';
+import './VideoList.scss';
 
 const VideoList = ({ videoData, selected, handleClick }) => {
     return (
         <section className="video-list">
             <h2 className="video-list__title">NEXT VIDEO</h2>
-            {videoData.map(video => {
-                if (video !== selected) {
-                    return (
-                        <Link to={`/video/${video.id}`} key={video.id}>
-                            <VideoListCard
-                                videoData={video}
-                                handleClick={() => handleClick(video.id)} />
-                        </Link>
-                    );
-                } else {
-                    return null;
-                }
-            })}
+            {videoData
+                .filter(video => video.id !== selected?.id)
+                .map(video => (
+                    <Link to={`/video/${video.id}`} key={video.id}>
+                        <VideoListCard
+                            videoData={video}
+                            handleClick={() => handleClick(video.id)} />
+                    </Link>
+                ))
+            }
         </section>
-    )
+    );
 }
 
-export default VideoList
+export default VideoList;
