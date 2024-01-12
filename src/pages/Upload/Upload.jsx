ea4d244a52cbd13c67e1df13a0f07e21ef35d1cf
Alireza Kiaee <45744021+alirezakiaee@users.noreply.github.com>
2024-01-12 16:50:58 -0500

    updated post method to axios (Previously practiced Fetch method)

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,4 +1,93 @@
+// *******************Practiced Fetch Method for posting
+// import React, { useState } from "react";
+// import './Upload.scss';
+// import thumbnail from "../../assets/images/Upload-video-preview.jpg";
+// import uploadIcon from "../../assets/icons/upload.svg";
+
+// const Upload = () => {
+//   const [formData, setFormData] = useState({
+//     title: '',
+//     description: ''
+//   });
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormData(prevFormData => ({
+//       ...prevFormData,
+//       [name]: value
+//     }));
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       const response = await fetch('http://localhost:8080/videos/', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(formData),
+//       });
+
+//       if (response.ok) {
+//         console.log('Video uploaded successfully');
+//       } else {
+//         console.error('Upload failed');
+//       }
+//     } catch (error) {
+//       console.error('Error:', error);
+//     }
+//   };
+
+//   return (
+//     <section className="upload">
+//       <header className="upload__margin">
+//         <h1 className="upload__title">Upload Video</h1>
+//       </header>
+//       <form className="upload__flex" onSubmit={handleSubmit}>
+//         <div className="upload__flex-two-left">
+//           <h4 className="upload__label">VIDEO THUMBNAIL</h4>
+//           <img className="upload__image" src={thumbnail} alt="thumbnail" />
+//         </div>
+//         <div className="upload__flex-two-right">
+//           <label htmlFor="title" className="upload__label">TITLE YOUR VIDEO</label>
+//           <input
+//             id="title"
+//             className="upload__input"
+//             name="title"
+//             placeholder="Add a title to your video"
+//             autoComplete="off"
+//             value={formData.title}
+//             onChange={handleInputChange}
+//           />
+//           <label htmlFor="description" className="upload__label">ADD A VIDEO DESCRIPTION</label>
+//           <textarea
+//             id="description"
+//             className="upload__textarea"
+//             name="description"
+//             rows="10"
+//             placeholder="Add a description of your video"
+//             value={formData.description}
+//             onChange={handleInputChange}
+//           ></textarea>
+//         <footer className="upload__action">
+//           <button type="submit" className="upload__button-publish">
+//             <img className="search-form__submit-icon" src={uploadIcon} alt="upload icon" />
+//             PUBLISH
+//           </button>
+//           <button className="upload__link">CANCEL</button>
+//         </footer>
+//         </div>
+//       </form>
+//     </section>
+//   );
+// };
+// export default Upload;
+
+//Post with axios
+
 import React, { useState } from "react";
+import axios from 'axios';
 import './Upload.scss';
 import thumbnail from "../../assets/images/Upload-video-preview.jpg";
 import uploadIcon from "../../assets/icons/upload.svg";
@@ -20,21 +109,10 @@ const Upload = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:8080/videos/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        console.log('Video uploaded successfully');
-      } else {
-        console.error('Upload failed');
-      }
+      const response = await axios.post('http://localhost:8080/videos/', formData);
+      console.log('Video uploaded successfully', response.data);
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Upload failed', error);
     }
   };
 
